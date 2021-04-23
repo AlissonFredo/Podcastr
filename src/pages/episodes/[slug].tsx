fe6a4e78fcb9b040eb827136ba7348732a468364
alisson fredo <douglasalissonsf@gmail.com>
2021-04-23 03:38:25 -0300

    aula 04 - adicionei função de play e pause

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -25,8 +25,6 @@ type EpisodeProps = {
 };
 
 export default function Episode({episode}: EpisodeProps) {
-    const router = useRouter();
-
     return (
         <div className={styles.scroll}>
             <div className={styles.episode}>
@@ -59,8 +57,24 @@ export default function Episode({episode}: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const {data} = await api.get('episodes', {
+        params: {
+            _limit: 2,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    })
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+    
     return {
-        paths: [],
+        paths,
         fallback: 'blocking'
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 import styles from './home.module.scss';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 type Episode = {
   id: string;
@@ -24,6 +26,8 @@ type HomeProps = {
 }
 
 export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
+  const {play} = useContext(PlayerContext)
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -43,7 +47,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button>
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio"/>
                 </button>
               </li>
